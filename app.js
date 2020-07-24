a97a7e71191df3eedef5327f099a2175c20c999f
Rishabh Garg <rishabh.garg01@nagarro.com>
2020-07-24 20:27:55 +0530

    update test-cases

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-var server = app.listen(3000, () => {
+var server = app.listen(3001, () => {
     console.log("Listening on port " + server.address().port + "...");
 });
+
+module.exports = server;
---
+++ spec/app.spec.js
@@ -0,0 +1,27 @@
+var Request = require("request");
+
+describe("Server", () => {
+    var server;
+    beforeAll(() => {
+        server = require("../app");
+    });
+    afterAll(() => {
+        server.close();
+    });
+    describe("GET /", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3000/", (error, response, body) => {
+                data.status = response.statusCode;
+                data.body = body;
+                done();
+            });
+        });
+        it("Status 200", () => {
+            expect(data.status).toBe(200);
+        });
+        it("Body", () => {
+            expect(data.body).toBe("Hello World!");
+        });
+    });
+});
